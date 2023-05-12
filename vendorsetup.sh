2122809f4c2caba43d2dd0f91210b48553e62c1c
zacky46 <zackyma777@gmail.com>
2023-05-12 14:33:48 +0000

    lavender: update config

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,7 @@
 #
 #	This file is part of the OrangeFox Recovery Project
 # 	Copyright (C) 2019-2023 The OrangeFox Recovery Project
-#	
+#
 #	OrangeFox is free software: you can redistribute it and/or modify
 #	it under the terms of the GNU General Public License as published by
 #	the Free Software Foundation, either version 3 of the License, or
@@ -14,33 +14,22 @@
 #
 # 	This software is released under GPL version 3 or any later version.
 #	See <http://www.gnu.org/licenses/>.
-# 	
+#
 # 	Please maintain this if you use this script or any part of it
 #
-FDEVICE="lavender"
-
-fox_get_target_device() {
-local chkdev=$(echo "$BASH_SOURCE" | grep \"$FDEVICE\")
-   if [ -n "$chkdev" ]; then
-      FOX_BUILD_DEVICE="$FDEVICE"
-   else
-      chkdev=$(set | grep BASH_ARGV | grep \"$FDEVICE\")
-      [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
-   fi
-}
-
-if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
-   fox_get_target_device
-fi
-
-if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+        export LC_ALL="C"
+        export DATE=$(sed "s/://g" <<< $(date +%y%m%d_%T))
+        export FOX_VERSION="R11.1"
+        export FOX_BUILD_TYPE="Unofficial"
    	export TW_DEFAULT_LANGUAGE="en"
 	export OF_KEEP_FORCED_ENCRYPTION=1
+        export OF_FIX_DECRYPTION_ON_DATA_MEDIA=1
+        export OF_SKIP_DECRYPTED_ADOPTED_STORAGE=1
 	export FOX_ENABLE_APP_MANAGER=1
         export OF_SCREEN_H=2340
         export OF_STATUS_H=80
-        export OF_STATUS_INDENT_LEFT=48
-        export OF_STATUS_INDENT_RIGHT=48
+        export OF_STATUS_INDENT_LEFT=58
+        export OF_STATUS_INDENT_RIGHT=58
   	export OF_HIDE_NOTCH=1
   	export OF_CLOCK_POS=1
 	export OF_USE_MAGISKBOOT=1
@@ -77,13 +66,3 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 
 	# run a process after formatting data to work-around MTP issues
 	export OF_RUN_POST_FORMAT_PROCESS=1
-
-	# let's see what are our build VARs
-	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
-  	   export | grep "FOX" >> $FOX_BUILD_LOG_FILE
-  	   export | grep "OF_" >> $FOX_BUILD_LOG_FILE
-   	   export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
-  	   export | grep "TW_" >> $FOX_BUILD_LOG_FILE
- 	fi
-fi
-#
